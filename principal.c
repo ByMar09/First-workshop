65fcf5c7aa579388ce39d91d929d61562bd86ad8
First-workshop <bmuniandlinux@gmmail.com>
2024-02-26 15:59:10 -0500

    Función menu

--- principal.c
+++ principal.c
@@ -1,1 +1,12 @@
 #include<iostream>
+
+int menu()
+{
+    int selection;
+    std::cout<<":::::::::::::::Bienvenido a la calculadora:::::::::::::::"<<std::endl;
+    std::cout<<"\t1. Suma \n\t2. Resta \n\t3. Multiplicación   \n\t4. División \n\t5. Módulo"<<std::endl;
+    std::cin>>selection;
+    system("clear");
+    return selection; 
+}
+
