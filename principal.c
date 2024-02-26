33d95f7db8265e581eb25ee67a20f1de2f1a5716
First-workshop <bmuniandlinux@gmmail.com>
2024-02-26 16:18:48 -0500

    Función principal

--- principal.c
+++ principal.c
@@ -36,3 +36,32 @@ double div(double num1, double num2){
 double mod(double num1, double num2){
     return(num1%num2);
 }
+
+int main(){
+    double x,y;
+    x=read();
+    y=read();
+    switch(menu())
+    {
+        case 1:
+            std::cout<<"El resultado de la suma es: "<<suma(x,y)<<std::endl;
+        break;
+        case 2:
+            std::cout<<"El resultado de la resta es: "<<resta(x,y)<<std::endl;
+        break;
+        case 3:
+            std::cout<<"El resultado de la multiplicación es: "<<mult(x,y)<<std::endl;
+        break;
+        case 4:
+            std::cout<<"El resultado de la divión es: "<<div(x,y)<<std::endl;
+        break;
+        case 5:
+            std::cout<<"El módulo es: "<<mod(x,y)<<std::endl;
+        break;
+        default:
+            std::cout<<"Opción inválida"<<std::endl;
+        break;
+    }
+
+    return 0;
+}
